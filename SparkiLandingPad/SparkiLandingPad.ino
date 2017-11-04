5650671154342775cd3fd18e1a24eeff831e0a23
Jarrett Philips <jarrettphilips@gmail.com>
2017-11-04 13:43:16 -0600

    Update SparkiLandingPad.ino

--- SparkiLandingPad/SparkiLandingPad.ino
+++ SparkiLandingPad/SparkiLandingPad.ino
@@ -1,25 +1,38 @@
 #include <Sparki.h>
 
+//Motor Variables
+int rightMotorSpeed = 0;
+int leftMotorSpeed = 0;
+
 //Timing Variables
 int loopCycles = 0;
 int totalElapsedTime = 0;
 int delayTime = 0;
 
 //State Variables
-int state = 0;
-int randomlyChooseState = 1; //1 true, 0 false
+int state = 1;
+int randomlyChooseState = 0; //1 true, 0 false
 
 void setup(){
-  //Randomly selects a state, unless overwritten
+  if(randomlyChooseState){
+    state = random(1, 4); //Randomly selects a state, unless overwritten
+  }
   
+  if(state == 1){
+    int speed = random(1, 101);
+    rightMotorSpeed = speed;
+    leftMotorSpeed = speed;
+  }
 }
 
-void loop() {
+void loop(){
   //State Machine
   switch(state){
     case 1 : //Linear Path of Variable Speed
+      sparki.motorRotate(MOTOR_RIGHT, DIR_CW,  rightMotorSpeed);
+      sparki.motorRotate(MOTOR_LEFT, DIR_CCW,  leftMotorSpeed);
       break;
-    case 2 : //Cicular Path of Variable Speed
+    case 2 : //Circular Path of Variable Speed
       break;
     case 3 : //Sin Path of Variable Speed
       break;
