9f561955104bdaf2083fc52df079dd8605a6ba52
Jarrett Philips <jarrettphilips@engr2-19-83-dhcp.int.colorado.edu>
2017-11-08 17:29:06 -0700

    pathetic openCV code start

---
+++ Drone/OpenCV.py
@@ -0,0 +1,11 @@
+'''
+	Command Line Arguements:
+	>python OpenCV.py photoDirectory 
+'''
+
+import opencvmaster
+import sys
+
+photoDirectory = sys.argv[1]
+print("Running diagnostics on file:", photoDirectory)
+
