ec04ab5f2ccf06057df4c430bc7947959050809a
Jarrett Philips <jarrettphilips@gmail.com>
2017-11-13 08:25:26 -0700

    Update OpenCV.py

--- Drone/OpenCV.py
+++ Drone/OpenCV.py
@@ -18,28 +18,28 @@ paramSquare = cv.SimpleBlobDetector_Params()
 paramCircle = cv.SimpleBlobDetector_Params()
 
 #Change thresholds
-paramSquare.minThreshold = 10;
-paramSquare.maxThreshold = 200;
-paramCircle.minThreshold = 10;
-paramCircle.maxThreshold = 200;
+paramSquare.minThreshold = 10
+paramSquare.maxThreshold = 200
+paramCircle.minThreshold = 10
+paramCircle.maxThreshold = 200
  
-# Filter by Area.
-paramSquare.filterByArea = true;
-paramSquare.minArea = 200;
-paramCircle.filterByArea = true;
-paramCircle.minArea = 200;
+# Filter by Area
+paramSquare.filterByArea = true
+paramSquare.minArea = 200
+paramCircle.filterByArea = true
+paramCircle.minArea = 200
  
 # Filter by Circularity
-paramSquare.filterByCircularity = true;
-paramSquare.minCircularity = 0.75;
-paramCircle.filterByCircularity = true;
-paramCircle.minCircularity = 0.95;
+paramSquare.filterByCircularity = true
+paramSquare.minCircularity = 0.75
+paramCircle.filterByCircularity = true
+paramCircle.minCircularity = 0.95
 
 # Filter by Inertia
-paramSquare.filterByInertia = true;
-paramSquare.minInertiaRatio = 0.75;
-paramCircle.filterByInertia = true;
-paramCircle.minInertiaRatio = 0.75;
+paramSquare.filterByInertia = true
+paramSquare.minInertiaRatio = 0.75
+paramCircle.filterByInertia = true
+paramCircle.minInertiaRatio = 0.75
 
 
 # Create Detectors
