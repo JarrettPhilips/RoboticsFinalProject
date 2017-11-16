0669f7aa98894541df0d3e24506ac6d91be848f6
Jarrett Philips <jarrettphilips@engr2gw-0-57-dhcp.int.colorado.edu>
2017-11-15 18:10:16 -0700

    completed procedural code

--- Drone/OpenCV.py
+++ Drone/OpenCV.py
@@ -4,7 +4,7 @@
 	Drone OpenCV Code - Find landing pad and calculate drone's position in relation to it
 
 	Command Line Arguements:
-	>python OpenCV.py photoDirectory 
+	>python OpenCV.py photoDirectory, altitude (cm)
 	
 	Dependencies:
 	- Place OpenCV library in same directory as python file
@@ -14,8 +14,9 @@ import opencvmaster as cv
 import numpy as np
 import sys	
 
-
-########## Global Variables ##########
+#####################################################
+'''				 	  Variables			  	      '''
+#####################################################
 #Landing Pad
 innerCircleTrueRadius = 2.54 #cm (1 inch)
 middleCircleTrueRadius = 7.62 #cm (3 inch)
@@ -28,13 +29,27 @@ photoWidthFromOneMeter = 187.1 #cm (the width of the rectangle that the drone's 
 photoHeightFromOneMeter = 101.2
 
 #Drone
-altitude = 0 #cm
+altitude = 0.0 #cm
 
+#####################################################
+'''				 Procedural / Main			  	  '''
 #####################################################
 
 photoDirectory = sys.argv[1]
+altitude = sys.argv[2]
 print("Running diagnostics on file:", photoDirectory)
 
+photoX, photoY = findPad(detectBlobs(photoDirectory))
+print("PhotoX:", photoX, "| PhotoY:", photoY)
+pixelX, pixelY = convertPhotoCoordinates(photoX, photoY)
+print("PixelX:", PixelX, "| PixelY:", pixelY)
+relativeX, relativeY = calculatePadLocation(pixelX, pixelY)
+print("RelativeX:", relativeX, "| RelativeY:", relativeY)
+
+return relativeX, relativeY
+
+#####################################################
+'''				 	  Functions			  	  	  '''
 #####################################################
 def detectBlobs(path):
 	img = cv.imread(path)
@@ -64,15 +79,11 @@ def detectBlobs(path):
 	blobs = detector.detect(img)
 	return blobs
 
-def findPad(blobs):
+def findPad(blobs): #returns x, y coordinates (photo coordinates)
 	for i in blobs
 	# make a dictionary with the coordinates of blobs as the key
 	# and the key with 3 entries is the pad
 
-
-
-
-# Calculate Distance to Pad
 def calculatePadLocation(innerCirclePixelOriginX, innerCirclePixelOriginY): #Returns x,y coordinates of pad RELATIVE to drone (cm)
 	landingPadRelativeX = (innerCirclePixelOriginX / photoWidth) * (photoWidthFromOneMeter / 100) * (altitude)
 	landingPadRelativeY = (innerCirclePixelOriginY / photoHeight) * (photoHeightFromOneMeter / 100) * (altitude)
