f6bbf00e1028931922b7f4bacff5ee4aeaf55cc9
masc7859 <masc7859@colorado.edu>
2017-11-27 01:37:05 -0700

    fixed submodule issue

--- drone_project/drone_ws/build/bebop_autonomy/bebop_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Documents/drone/drone_project/drone_ws/src/bebop_autonomy/bebop_driver
-# Build directory: /home/user/Documents/drone/drone_project/drone_ws/build/bebop_autonomy/bebop_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
