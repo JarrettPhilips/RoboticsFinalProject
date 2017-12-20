7e497492941d9f642d4991c3fbc5be20e63f965e
Anthony Keydel <31680978+AnthonyKeydel@users.noreply.github.com>
2017-12-19 17:16:55 -0700

    Update SparkiLandingPad.ino

--- Sparki/SparkiLandingPad.ino
+++ Sparki/SparkiLandingPad.ino
@@ -18,14 +18,37 @@ int state = 1;
 int randomlyChooseState = 1; //1 true, 0 false
 int randomlyChooseStateVariables = 1;
 
+void playFanfare(){
+  int melody[] = {NOTE_A5 , NOTE_A5, NOTE_A5, NOTE_A5, NOTE_F5, NOTE_G, NOTE_A, NOTE_G, NOTE_A}
+  int noteDurations[] = {8, 8, 8, 4, 3, 8, 8, 4, 8, 2}
+  
+  for (int thisNote = 0; thisNote < melody.size(); thisNote++) {
+ 
+    // calculate the note duration as 1 second divided by note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    sparki.beep(melody[thisNote],noteDuration);
+    }
+ 
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.20;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    sparki.noBeep();
+  }
+  
+  
+}
+
 void setup(){
   randomSeed(analogRead(0)); 
   if(randomlyChooseState){
-    state = random(1, 4); //Randomly selects a state, unless overwritten
+    state = random(1, 3); //Randomly selects a state, unless overwritten
   }
   
   if(randomlyChooseStateVariables){
-    speed = random(1, 101);
+    speed = random(50, 101);
     rightMotorSpeed = speed;
     leftMotorSpeed = speed;
   
@@ -41,20 +64,17 @@ void setup(){
 }
 
 void loop(){
-  //State Machine (Depending on how we implement the sin path, we may not even need this)
-  switch(state){
-    case 1 : //Linear Path of Variable Speed
-      sparki.motorRotate(MOTOR_RIGHT, DIR_CW,  rightMotorSpeed);
-      sparki.motorRotate(MOTOR_LEFT, DIR_CCW,  leftMotorSpeed);
-      break;
-    case 2 : //Circular Path of Variable Speed and Radius (will always go CCW)
-      sparki.motorRotate(MOTOR_RIGHT, DIR_CW,  rightMotorSpeed);
-      sparki.motorRotate(MOTOR_LEFT, DIR_CCW,  leftMotorSpeed);
-      break;
-    case 3 : //Sin Path of Variable Speed
-      break;
-  }
+  sparki.motorRotate(MOTOR_RIGHT, DIR_CW,  rightMotorSpeed);
+  sparki.motorRotate(MOTOR_LEFT, DIR_CCW,  leftMotorSpeed);
   
+  #testcode for beep when drone is caught
+  if(sparki.accelY() < -1){
+      sparki.moveStop();  
+      playFanfare();
+      delay(-1)
+  }
+    
+        
   //Timing Code
   int loopTime = millis() - totalElapsedTime;
   if((adjustedLoopTime + delayTime) - loopTime < 0){
